505516c2b4b4cef068d938266ede9fe2edf7389d
Anton <antonklimowicz@gmail.com>
2022-05-03 10:55:08 +0200

    Account activation

--- src/server/models/token-model.js
+++ src/server/models/token-model.js
@@ -1,8 +1,8 @@
 const {Schema, model} = require('mongoose');
 
 const TokenSchema = new Schema({
-    user: {type: Schema.Types.ObjectId, ref: 'User'},
+    user: {type: Schema.Types.ObjectId, ref: 'Token'},
     refreshToken: {type: String, required: true},
 });
 
-module.exports = model('User', UserSchema);
+module.exports = model('Token', TokenSchema);
